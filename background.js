fbc1162183beec2fde12f995b7300e6894f28ee0
Rohan Panuganti <panugant@seattleu.edu>
2020-12-20 12:45:02 -0800

    no more checking youtube url in background.js

--- background.js
+++ background.js
@@ -1,21 +1,7 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    if (validate(changeInfo.url)) {
-        chrome.tabs.executeScript(tab.id, { file: "inject.js" });
-    }
+    chrome.tabs.executeScript(tab.id, { file: "inject.js" });
 });
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-    if (validate(tab.url)) {
-        chrome.tabs.executeScript(tab.id, { file: "inject.js" });
-    }
+    chrome.tabs.executeScript(tab.id, { file: "inject.js" });
 });
-
-function validate(url){
-    if (url) {
-        var regExp = /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
-        if (url.match(regExp)) {
-            return true;
-        }
-    }
-    return false;
-}
